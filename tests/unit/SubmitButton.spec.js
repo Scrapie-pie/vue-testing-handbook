61d4944c14511c29fc246a00a20382dfd03907e0
Sergey Bochkarev <neontec2@gmail.com>
2023-08-07 11:46:49 +0300

    Рефакторинг через фабрику (factory function)

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -1,30 +1,32 @@
 import { mount } from '@vue/test-utils'
 import SubmitButton from '@/components/SubmitButton.vue'
 
-describe('SubmitButton.vue', () => {
-  it("Отображает сообщение для неавторизованного пользователя", () => {
-    const msg = "Войти"
-    const wrapper = mount(SubmitButton,{
-      props: {
-        msg: msg
-      }
-    })
+const msg = "Войти"
+const factory = (props) => {
+  return mount(SubmitButton, {
+    props: {
+      msg,
+      ...props
+    }
+  })
+}
 
-    expect(wrapper.find("span").text()).toBe("Не авторизован")
-    expect(wrapper.find("button").text()).toBe("Войти")
+describe("SubmitButton.vue", () => {
+  describe("Нет привилегий администратора", ()=> {
+    it("Выводит верное сообщение", () => {
+      const wrapper = factory()
+
+      expect(wrapper.find("span").text()).toBe("Не авторизован")
+      expect(wrapper.find("button").text()).toBe("Войти")
+    })
   })
 
-  it('Отображает сообщение для администратора', () => {
-    const msg = "Войти"
-    const isAdmin = true
-    const wrapper = mount(SubmitButton,{
-      props: {
-        msg,
-        isAdmin
-      }
-    })
+  describe("Есть привилегии администратора", ()=> {
+    it("Выводит верное сообщение", () => {
+      const wrapper = factory({ isAdmin: true })
 
-    expect(wrapper.find("span").text()).toBe("Привилегии администратора")
-    expect(wrapper.find("button").text()).toBe("Войти")
+      expect(wrapper.find("span").text()).toBe("Привилегии администратора")
+      expect(wrapper.find("button").text()).toBe("Войти")
+    })
   })
 })
